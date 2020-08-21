e0d8415581f09c1020feaac8caab0e7ec80ac721
claytoncalixto <claytonrcalixto@gmail.com>
2020-08-21 18:56:38 -0300

    Obtendo um usuário por id
    Obtendo um usuário por id

--- src/main/java/com/claytoncalixto/workshopmongo/services/UserService.java
+++ src/main/java/com/claytoncalixto/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.claytoncalixto.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.claytoncalixto.workshopmongo.domain.User;
 import com.claytoncalixto.workshopmongo.repository.UserRepository;
+import com.claytoncalixto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
