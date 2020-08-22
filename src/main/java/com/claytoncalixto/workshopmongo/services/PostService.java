34b554c5bf3308b1ac8c5e7e45ddc09590fb0884
claytoncalixto <claytonrcalixto@gmail.com>
2020-08-22 20:42:48 -0300

    Obtendo um post por id
    Obtendo um post por id

---
+++ src/main/java/com/claytoncalixto/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.claytoncalixto.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claytoncalixto.workshopmongo.domain.Post;
+import com.claytoncalixto.workshopmongo.repository.PostRepository;
+import com.claytoncalixto.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
