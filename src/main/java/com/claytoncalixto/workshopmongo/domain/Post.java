3b6329e6934dce94a5c0940dc8e8799cae590915
claytoncalixto <claytonrcalixto@gmail.com>
2020-08-24 19:09:33 -0300

    Acrescentando comentários aos posts
    Acrescentando comentários aos posts

--- src/main/java/com/claytoncalixto/workshopmongo/domain/Post.java
+++ src/main/java/com/claytoncalixto/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.claytoncalixto.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.claytoncalixto.workshopmongo.dto.AuthorDTO;
+import com.claytoncalixto.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -20,6 +23,8 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -73,6 +78,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
