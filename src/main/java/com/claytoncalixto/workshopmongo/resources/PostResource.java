116619a9a1e40317951dd6ba07242f10647a8466
claytoncalixto <claytonrcalixto@gmail.com>
2020-08-25 19:50:43 -0300

    Consulta com vários critérios
    Consulta com vários critérios

--- src/main/java/com/claytoncalixto/workshopmongo/resources/PostResource.java
+++ src/main/java/com/claytoncalixto/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.claytoncalixto.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +18,30 @@ import com.claytoncalixto.workshopmongo.services.PostService;
 @RestController
 @RequestMapping(value = "/posts")
 public class PostResource {
-	
+
 	@Autowired
-	private PostService service;
-	
-	
-	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Post> findById(@PathVariable String id){
+	private PostService service;   
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(value= "/titlesearch", method=RequestMethod.GET)
-	public ResponseEntity<List> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+
+	@RequestMapping(value = "/fullsearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeparam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeparam(text);
 		List<Post> list = service.findByTitle(text);
 		return ResponseEntity.ok().body(list);
